36b6e7a5e9af1bf8bd66a1a6718ea03d73e67ccf
Sze-Hong-Cheung <Jesse@JessedeMacBook-Pro.local>
2020-08-25 02:35:57 +0800

    Updates

---
+++ precache-manifest.cc18807d59c34d8c9684972378f5a823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822d59afc2e18adf6f158dabe643727c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d13de9ea103e986b61cd",
+    "url": "/monsters-rolodex/static/css/main.cc2850e8.chunk.css"
+  },
+  {
+    "revision": "4cc4f5966468c8380459",
+    "url": "/monsters-rolodex/static/js/2.8f4e6376.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/monsters-rolodex/static/js/2.8f4e6376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13de9ea103e986b61cd",
+    "url": "/monsters-rolodex/static/js/main.277affa5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
